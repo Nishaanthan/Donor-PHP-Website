93988e4ae712cc40a4f7939959702cf0a53e8114
Nisanthan K <Nisanthan@email.com>
2019-08-30 23:02:41 +0530

    Corrected errors with Donor Registration and Sql file added

--- Sign up Donor/register.php
+++ Sign up Donor/register.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 /* Registration process, inserts user info into the database 
    and sends account confirmation email message
  */
